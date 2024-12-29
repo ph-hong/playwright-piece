175888d80f8227fe4d67681272468f86b1985bf7
Hong Phan <ptthong21676@gmail.com>
2024-12-29 16:08:56 +0800

    day-01 Variables

---
+++ lessons/day-01/Variables.js
@@ -0,0 +1,20 @@
+// Declaration
+let a = 1;
+
+// Re-assign is acceptable
+a = 2;
+
+// Constant
+const PI_NUMBER = 3.14;
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+// Check the type
+console.log(typeof(1.7976931348623157e+308 + 1))
+console.log(Infinity > 1.7976931348623157e+308);
+console.log(0.1 + 0.2);
+
+//Boolean
+let amIright = false;
+console.log(typeof amIright);
