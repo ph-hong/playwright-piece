e1143bbbb369c99955f976437188e217a9ff4d60
Hong Phan <ptthong21676@gmail.com>
2025-01-19 23:52:48 +0800

    Add swap function to ArrayHandle

--- labs/utils/ArrayHandle.js
+++ labs/utils/ArrayHandle.js
@@ -2,7 +2,13 @@ function isEvenNumber(number) {
     return number % 2 === 0
 };
 
+function swap(arr, j) {
+    let temp = arr[j];
+    arr[j] = arr[j + 1];
+    arr[j + 1] = temp;
+}
 
 module.exports = {
     isEvenNumber,
+    swap,
 };
