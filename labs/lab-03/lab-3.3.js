f64eff573fef1c7f381252a265ea65879cd789f1
Hong Phan <ptthong21676@gmail.com>
2025-01-19 23:53:26 +0800

    [lab-03] update 3.3

--- labs/lab-03/lab-3.3.js
+++ labs/lab-03/lab-3.3.js
@@ -4,14 +4,29 @@ Input: [12, 34, 1, 16, 28]
 Expected output: [1, 12, 16, 28, 34]
  */
 
+const { swap } = require('../utils/ArrayHandle.js');
 let arr = [12, 34, 1, 16, 28];
+let len = arr.length;
 
-for (let index = 0; index < arr.length; index++) {
-    if (arr[index] > arr[index + 1]) {
-        let temp = arr[index];
-        arr[index] = arr[index + 1];
-        arr[index + 1] = temp;
-        index = -1;
+// for (let index = 0; index < arrLength; index++) {
+//     if (arr[index] > arr[index + 1]) {
+//         let temp = arr[index];
+//         arr[index] = arr[index + 1];
+//         arr[index + 1] = temp;
+//         index = -1;
+//     }
+// }
+
+for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+        const leftValue = arr[j];
+        const rightValue = arr[j + 1];
+        const isWrongPlace = leftValue > rightValue;
+
+        if (isWrongPlace) {
+            swap(arr, j)
+        }
     }
 }
+
 console.log(`Sort array: ${arr}`)
