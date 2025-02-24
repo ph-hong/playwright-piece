e5f59678280ee67bcc91317f89016cf770d04c76
Hong Phan <ptthong21676@gmail.com>
2025-02-24 23:39:57 +0800

    day-10 String continue

---
+++ lessons/day-09/StringLearningCnt.js
@@ -0,0 +1,14 @@
+// 1. Sub string
+let strToBeCut = "https://developer.mozilla.org/en-US/";
+
+// Inclusive
+let startIndex = 0;
+
+// Exclusive
+let endIndex = 16;
+
+let subString = strToBeCut.substring(startIndex, endIndex);
+console.log(subString)
+
+// 2. Split string
+console.log(strToBeCut.split(""));
