2996343523ccffcd58c7b977347df42ebd58435f
Hong Phan <ptthong21676@gmail.com>
2025-04-01 20:22:50 +0800

    [lab-09] code refactoring

--- labs/lab-09/EmployeeManagement.js
+++ labs/lab-09/EmployeeManagement.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var ContructorE_1 = require("./ContructorE");
+var ContractorE_1 = require("./ContractorE");
 var FTE_1 = require("./FTE");
 var EmployeeController_1 = require("./EmployeeController");
 var employees = [
-    new ContructorE_1.default('Con-01', 12 * 1000),
-    new ContructorE_1.default('Con-02', 10 * 1000),
+    new ContractorE_1.default('Con-01', 12 * 1000),
+    new ContractorE_1.default('Con-02', 10 * 1000),
     new FTE_1.default('Fte-01', 51 * 1000),
     new FTE_1.default('Fte-03', 52 * 1000),
     new FTE_1.default('Fte-02', 53 * 1000)
--- labs/lab-09/EmployeeManagement.ts
+++ labs/lab-09/EmployeeManagement.ts
@@ -1,10 +1,10 @@
-import ContructorEmp from "./ContructorE";
+import ContractorEmp from "./ContractorE";
 import FTE from "./FTE";
 import EmployeeController from "./EmployeeController";
 
 const employees = [
-    new ContructorEmp('Con-01', 12 * 1000),
-    new ContructorEmp('Con-02', 10 * 1000),
+    new ContractorEmp('Con-01', 12 * 1000),
+    new ContractorEmp('Con-02', 10 * 1000),
     new FTE('Fte-01', 51 * 1000),
     new FTE('Fte-03', 52 * 1000),
     new FTE('Fte-02', 53 * 1000)
