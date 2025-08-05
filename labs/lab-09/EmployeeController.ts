a2ed29cbeab20aeb0e61ea80dc7c49bf580bfd90
Hong Phan <ptthong21676@gmail.com>
2025-08-05 19:51:35 +0700

    [Lab-09 Code refactoring, add sortByName and sortBySalary

--- labs/lab-09/EmployeeController.ts
+++ labs/lab-09/EmployeeController.ts
@@ -1,8 +1,9 @@
 import Employee from "./Employee";
+import { ASC, DESC } from "./SortRuleType"
 
 export default class EmployeeController {
 
-    static getTotalSalary(employeeList: Employee[]): number {
+    getTotalSalary(employeeList: Employee[]): number {
         let totalSalary = 0;
         for (const employee of employeeList) {
             totalSalary += employee.getSalary();
@@ -10,7 +11,7 @@ export default class EmployeeController {
         return totalSalary;
     }
 
-    static getHighestSalaryEmp(employeeList: Employee[]) {
+    getHighestSalaryEmp(employeeList: Employee[]) {
         let highestSalary = employeeList[0];
         for (const employee of employeeList) {
             if (employee.getSalary() > highestSalary.getSalary()) {
@@ -20,4 +21,30 @@ export default class EmployeeController {
         return highestSalary;
     }
 
+    sortByName(employeeList: Employee[], rule: string): Employee[] {
+        if (rule === ASC) {
+            return employeeList.sort((a, b) => a.getName().localeCompare(b.getName()));
+        } else if (rule === DESC) {
+            return employeeList.sort((a, b) => b.getName().localeCompare(a.getName()));
+        } else {
+            this.printInvalidRule
+            return employeeList;
+        }
+    }
+
+    sortBySalary(employeeList: Employee[], rule: string): Employee[] {
+        if (rule === ASC) {
+            return employeeList.sort((a, b) => a.getSalary() - (b.getSalary()));
+        } else if (rule === DESC) {
+            return employeeList.sort((a, b) => b.getSalary() - (a.getSalary()));
+        } else {
+            this.printInvalidRule;
+            return employeeList
+        }
+    }
+
+    private printInvalidRule() {
+        console.error("Invalid rule");
+
+    }
 }
--- labs/lab-09/EmployeeManagement.ts
+++ labs/lab-09/EmployeeManagement.ts
@@ -1,6 +1,7 @@
 import ContractorEmp from "./ContractorE";
 import FTE from "./FTE";
 import EmployeeController from "./EmployeeController";
+import { ASC, DESC } from "./SortRuleType"
 
 const employees = [
     new ContractorEmp('Con-01', 12 * 1000),
@@ -10,8 +11,19 @@ const employees = [
     new FTE('Fte-02', 53 * 1000)
 ];
 
-const totalSalary = EmployeeController.getTotalSalary(employees);
-const highestSalaryEmp = EmployeeController.getHighestSalaryEmp(employees);
+const controller = new EmployeeController();
+const totalSalary = controller.getTotalSalary(employees);
+const highestSalaryEmp = controller.getHighestSalaryEmp(employees);
 
 console.log(`Total salary: ${totalSalary}`);
 console.log(`${highestSalaryEmp.getName()} has the highest salary of ${highestSalaryEmp.getSalary()}`);
+
+const sortedByNameASC = controller.sortByName(employees, ASC);
+console.log(`===1===Sorted by name ASC: ${JSON.stringify(sortedByNameASC)}`);
+const sortedByNameDESC = controller.sortByName(employees, DESC);
+console.log(`===1===Sorted by name DESC: ${JSON.stringify(sortedByNameDESC)}`);
+
+const sortedBySalaryASC = controller.sortBySalary(employees, ASC);
+console.log(`===2===Sorted by salary ASC: ${JSON.stringify(sortedBySalaryASC)}`);
+const sortedBySalaryDESC = controller.sortBySalary(employees, DESC);
+console.log(`===Sorted by salary DESC: ${JSON.stringify(sortedBySalaryDESC)}`);
